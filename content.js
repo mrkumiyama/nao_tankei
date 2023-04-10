57ed9fb71e78e41c5ac8996edf24873f51ecb76a
mrkumiyama <130334944+mrkumiyama@users.noreply.github.com>
2023-04-10 13:16:33 +0900

    First commit
    Projeto

---
+++ content.js
@@ -0,0 +1,31 @@
+const substituteWords = ['Cuzil', 'Bostil', 'Depressil', 'Macaquil', 'Chipanzil', 'Carioquil', 'Pitibulzil', 'Malcriadil', 'Merdil'];
+
+function replaceText(node) {
+  const regex = /Brasil/gi;
+  const text = node.nodeValue;
+  const newText = text.replace(regex, () => {
+    const randomIndex = Math.floor(Math.random() * substituteWords.length);
+    return substituteWords[randomIndex];
+  });
+
+  if (newText !== text) {
+    node.nodeValue = newText;
+  }
+}
+
+function walkDOM(node, callback) {
+  if (node) {
+    callback(node);
+    node = node.firstChild;
+    while (node) {
+      walkDOM(node, callback);
+      node = node.nextSibling;
+    }
+  }
+}
+
+walkDOM(document.body, (node) => {
+  if (node.nodeType === Node.TEXT_NODE) {
+    replaceText(node);
+  }
+});
