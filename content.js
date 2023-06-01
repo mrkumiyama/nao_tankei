ccaa45d2cd04417b717c13a388971108dea8b914
mrkumiyama <130334944+mrkumiyama@users.noreply.github.com>
2023-06-01 19:05:07 +0900

    Update content.js
    - Include "Brazil"
    - Change "normal" to "normar"

--- content.js
+++ content.js
@@ -1,15 +1,21 @@
 const substituteWords = ['Cuzil', 'Cupretil', 'Bostil', 'Pardil', 'Lixil', 'Depressil', 'Macaquil', 'Chipanzil', 'Carioquil', 'Pitibulzil', 'Malcriadil', 'Merdil'];
 
 function replaceText(node) {
-  const regex = /Brasil/gi;
-  const text = node.nodeValue;
-  const newText = text.replace(regex, () => {
+  let text = node.nodeValue;
+  
+  // Replace "Brasil" or "Brazil" with a random substitute word
+  const brazilRegex = /Brasil|Brazil/gi;
+  text = text.replace(brazilRegex, () => {
     const randomIndex = Math.floor(Math.random() * substituteWords.length);
     return substituteWords[randomIndex];
   });
+  
+  // Replace "normal" with "normar"
+  const normalRegex = /\bnormal\b/gi; 
+  text = text.replace(normalRegex, "normar");
 
-  if (newText !== text) {
-    node.nodeValue = newText;
+  if (text !== node.nodeValue) {
+    node.nodeValue = text;
   }
 }
 
